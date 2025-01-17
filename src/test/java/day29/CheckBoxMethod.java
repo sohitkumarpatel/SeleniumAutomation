82946d44a931ca9d9edba15d0949f9ea5abc67a6
Sohit <sohitpatel@Sohits-Air.lan>
2025-01-17 11:17:47 +0530

    day29 added

---
+++ src/test/java/day29/CheckBoxMethod.java
@@ -0,0 +1,19 @@
+package day29;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CheckBoxMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().maximize();
+
+	}
+
+}
